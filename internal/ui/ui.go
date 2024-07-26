f8a801c4fd5a799c4040f4ab20ee2e58ff766987
stevenletts <s.spencerletts@gmail.com>
2024-07-26 07:35:07 +0100

    chore: prepare todo items db

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -25,8 +25,23 @@ const (
 	ViewList
 )
 
+// system level config options should be entry view ie on run come to add or list
+// configure options for displaing in the todo item as in order of deadline, created, description, title or reverse
+
 func (m Model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
+	// create the database and the table
+	// connect app to database
+	// fetch titles and deadline and display in view list
+	// on enter into todo item we can display text created
+	//
+	//
+	// docker run --name todo-postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d --rm postgres:13.0
+	// below will persist
+	// docker run --name my-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d -v pgdata:/var/lib/postgresql/data postgres:13.0
+	//
+	// docs for the driver i have installed already
+	// https://github.com/jackc/pgx
+
 	return nil
 }
 
